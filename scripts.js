8ae190c8f69dc03a24a6c7a8348fc7e4f42050d8
Christian Bolik <cbolik@spotify.com>
2023-09-20 23:18:15 +0200

    Album fetch implemented

--- scripts.js
+++ scripts.js
@@ -55,7 +55,7 @@ const getRedirectURI = (loc) => {
 }
 
 // Update here, encode value copied from Spotify Dev via btoa() in browser console
-const SPOTIFY_CLIENT_ID = "Yzg0Zjc3Mzk4MGMyNDBmYjk3NDI3ZWVlZGNmYjY3NGM=";
+const SPOTIFY_CLIENT_ID = "MWIxZDQwYTYxM2U1NDdmMGE2ZjNiMGVjN2U2ZDExNDE=";
 const SPOTIFY_REDIRECT_URI = getRedirectURI(window.location);
 
 const spotifyCheckForCurrentTrack = () => {
@@ -67,7 +67,7 @@ const spotifyCheckForCurrentTrack = () => {
   if (access_token && state != null) {
     // get currently playing track
     let url = "https://api.spotify.com/v1/me/player/currently-playing";
-    url += "?market=from_token";
+    //url += "?market=from_token";
     fetch(url, {
       headers: {
         Authorization: "Bearer " + access_token,
@@ -95,7 +95,7 @@ const spotifyCheckForCurrentTrack = () => {
 
 const spotifyGetAccessToken = () => {
   // authorize user and get access token
-  let scope = "user-read-currently-playing user-read-recently-played";
+  let scope = "user-read-currently-playing user-read-recently-played user-library-read";
   let url = "https://accounts.spotify.com/authorize";
   let state = generateRandomString(16);
 
@@ -150,5 +150,97 @@ const populateFromSpotify = (attr_prefix) => {
   const releaseYear = releaseDate.split("-")[0];
 
   setInnerHTML("current_title", `${artist}: "${title}" from album "${album}" (released ${releaseDate})`)
+}
+
+const spotifyGetUsersSavedAlbums = async () => {
+  let offset = 0;
+  let limit = 50;
+  let totalItems = -1;
+  let allData = [];
+
+  let params = getHashParams();
+  let access_token = params.access_token,
+    state = params.state;
+
+  if (access_token && state != null) {
+    while (offset < totalItems || totalItems === -1) {
+      let url = "https://api.spotify.com/v1/me/albums";
+      url += "?market=from_token";
+      url += "&offset=" + (offset || 0);
+      url += "&limit=" + (limit || 20);
+      console.log(`Getting albums ${offset} to ${offset + limit - 1}`);
+
+      const response = await fetch(url, {
+        headers: {
+          Authorization: "Bearer " + access_token,
+          Accept: "application/json"
+        },
+      });
+      //f (response.status === 204) {
+        const data = await response.json();
+        if (!data) {
+          console.error("Failed to fetch data from get albums API.")
+          break;
+        }
+
+        if (totalItems === -1) {
+          totalItems = data.total;
+        }
+        allData = allData.concat(data.items);
+        offset += limit;
+      //}
+    }
+
+    populateAlbumsFromSpotify(allData);
+  } else {
+    spotifyGetAccessToken();
+  }
+}
+
+const spotifyGetUsersSavedAlbumsOld = () => {
+  let params = getHashParams();
+  let access_token = params.access_token,
+    state = params.state;
+
+  if (access_token && state != null) {
+    let url = "https://api.spotify.com/v1/me/albums";
+    url += "?market=from_token";
+    url += "&offset=" + (offset || 0);
+    url += "&limit=" + (limit || 20);
+
+    fetch(url, {
+      headers: {
+        Authorization: "Bearer " + access_token,
+        Accept: "application/json"
+      },
+    })
+      .then((resp) => {
+        if (resp.status === 204) {
+          return null;
+        } else {
+          return resp.json();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          populateAlbumsFromSpotify(data);
+        } else {
+          populateAlbumsFromSpotify(null);
+        }
+      });
+  } else {
+    spotifyGetAccessToken();
+  }
+}
+
+const populateAlbumsFromSpotify = (albums_resp) => {
+  let num_albums = 0;
+  if (albums_resp) {
+    num_albums = albums_resp.length
+    setInnerHTML("users_albums", `Albums go here. num_albums: ${num_albums}`)
+    
+  } else {
+    setInnerHTML("users_albums", `Albums go here. Huh, no saved albums found though.`)
+  }
 }
 
